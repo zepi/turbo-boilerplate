debb445214a6278e493666dd2e7f6ebecef7ee33
Matthias Zobrist <matthias.zobrist@zepi.net>
2015-11-12 08:15:59 +0100

    Moved the content from the file framework.php into app.php and deleted framework.php

--- app.php
+++ app.php
@@ -1,8 +1,21 @@
 <?php
 
-require_once(__DIR__ . '/framework.php');
+// Load the autoloader
+require 'vendor/autoload.php';
 
+// Start the framework
+$framework = \Zepi\Turbo\Framework::getFrameworkInstance(__DIR__);
+
+// Register the framework modules
+$framework->getModuleManager()->registerModuleDirectory(__DIR__ . '/vendor/');
+
+// Activate the framework core module
+$framework->getModuleManager()->activateModule('Zepi\\Core\\Defaults');
+$framework->getModuleManager()->activateModule('Zepi\\Core\\AccessControl', true);
+$framework->getModuleManager()->activateModule('Zepi\\Core\\Management', true);
 $framework->getModuleManager()->activateModule('Zepi\\Web\\AccessControl', true);
+
+// Activate project modules
 $framework->getModuleManager()->activateModule('Zepi\\Web\\Starter', true);
 
 // Execute the main work
