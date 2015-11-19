8568dff12705fd102fd8c650735e4c6d7623b4bb
Matthias Zobrist <matthias.zobrist@zepi.net>
2015-11-19 13:08:46 +0100

    Added the starter module

---
+++ modules/Zepi/Starter/Module.php
@@ -0,0 +1,118 @@
+<?php
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 zepi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+/**
+ * zepi Turbo Starter
+ * 
+ * @package Zepi\Web\Test
+ * @author Matthias Zobrist <matthias.zobrist@zepi.net>
+ * @copyright Copyright (c) 2015 zepi
+ */
+
+namespace Zepi\Starter;
+
+use \Zepi\Turbo\Module\ModuleAbstract;
+
+/**
+ * zepi Turbo Starter
+ * 
+ * @author Matthias Zobrist <matthias.zobrist@zepi.net>
+ * @copyright Copyright (c) 2015 zepi
+ */
+class Module extends ModuleAbstract
+{
+    /**
+     * Initializes and return an instance of the given class name.
+     * 
+     * @access public
+     * @param string $className
+     * @return mixed
+     */
+    public function getInstance($className)
+    {
+        
+    }
+    
+    /**
+     * Initializes the module
+     * 
+     * @access public
+     */
+    public function initialize()
+    {
+        $menuManager = $this->_framework->getInstance('\\Zepi\\Web\\General\\Manager\\MenuManager');
+        
+        $menuEntry = new \Zepi\Web\General\Entity\MenuEntry(
+            'home',
+            '',
+            '/',
+            'mdi-home'
+        );
+        $menuManager->addMenuEntry('menu-left', $menuEntry);
+    }
+    
+    /**
+     * This action will be executed on the activation of the module
+     * 
+     * @access public
+     * @param string $versionNumber
+     * @param string $oldVersionNumber
+     */
+    public function activate($versionNumber, $oldVersionNumber = '')
+    {
+        // Add the event handler
+        $runtimeManager = $this->_framework->getRuntimeManager();
+        $runtimeManager->addEventHandler('\\Zepi\\Web\\Starter\\Event\\Homepage', '\\Zepi\\Web\\Starter\\EventHandler\\Homepage');
+        
+        // Add the route
+        $routeManager = $this->_framework->getRouteManager();
+        $routeManager->addRoute('', '\\Zepi\\Web\\Starter\\Event\\Homepage');
+        
+        // Add the template
+        $templatesManager = $this->_framework->getInstance('\\Zepi\\Web\\General\\Manager\\TemplatesManager');
+        $templatesManager->addTemplate('\\Zepi\\Web\\Starter\\Templates\\Homepage', $this->_directory . '/templates/home.phtml');
+    }
+    
+    /**
+     * This action will be executed on the deactiviation of the module
+     * 
+     * @access public
+     */
+    public function deactivate()
+    {
+        // Add the event handler
+        $runtimeManager = $this->_framework->getRuntimeManager();
+        $runtimeManager->removeEventHandler('\\Zepi\\Web\\Starter\\Event\\Homepage', '\\Zepi\\Web\\Starter\\EventHandler\\Homepage');
+        
+        // Add the route
+        $routeManager = $this->_framework->getRouteManager();
+        $routeManager->removeRoute('', '\\Zepi\\Web\\Starter\\Event\\Homepage');
+        
+        // Add the template
+        $templatesManager = $this->_framework->getInstance('\\Zepi\\Web\\General\\Manager\\TemplatesManager');
+        $templatesManager->removeTemplate('\\Zepi\\Web\\Starter\\Templates\\Homepage', $this->_directory . '/templates/home.phtml');
+    }
+}
---
+++ modules/Zepi/Starter/src/EventHandler/Homepage.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 zepi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+/**
+ * Homepage
+ * 
+ * @package Zepi\Starter
+ * @subpackage EventHandler
+ * @author Matthias Zobrist <matthias.zobrist@zepi.net>
+ * @copyright Copyright (c) 2015 zepi
+ */
+
+namespace Zepi\Starter\EventHandler;
+
+use \Zepi\Turbo\FrameworkInterface\WebEventHandlerInterface;
+use \Zepi\Turbo\Framework;
+use \Zepi\Turbo\Request\RequestAbstract;
+use \Zepi\Turbo\Request\WebRequest;
+use \Zepi\Turbo\Response\Response;
+use \Zepi\Web\Test\Exception;
+
+/**
+ * Homepage
+ * 
+ * @author Matthias Zobrist <matthias.zobrist@zepi.net>
+ * @copyright Copyright (c) 2015 zepi
+ */
+class Homepage implements WebEventHandlerInterface
+{
+    /**
+     * Homepage
+     * 
+     * @access public
+     * @param \Zepi\Turbo\Framework $framework
+     * @param \Zepi\Turbo\Request\WebRequest $request
+     * @param \Zepi\Turbo\Response\Response $response
+     */
+    public function execute(Framework $framework, WebRequest $request, Response $response)
+    {
+        $templatesManager = $framework->getInstance('\\Zepi\\Web\\General\\Manager\\TemplatesManager');
+        $response->setOutput($templatesManager->renderTemplate('\\Zepi\\Web\\Starter\\Templates\\Homepage', $framework, $request, $response));
+    }
+}
