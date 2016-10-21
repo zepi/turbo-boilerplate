a3648c40c373539f08bda5c4b09be115bd33ec0b
Matthias Zobrist <matthias.zobrist@zepi.net>
2016-10-21 17:00:03 +0200

    Added the cli config for the doctrine cli class

---
+++ config/cli-config.php
@@ -0,0 +1,8 @@
+<?php
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+define('EXECUTION_DISABLED', true);
+require_once __DIR__ . '/../app.php';
+
+$entityManager = $framework->getInstance('\\Zepi\\DataSourceDriver\\Doctrine\\Manager\\EntityManager');
+return ConsoleRunner::createHelperSet($entityManager->getDoctrineEntityManager());
